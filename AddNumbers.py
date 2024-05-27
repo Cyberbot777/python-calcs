d8fc0700a91cdc58ca6ebb89e9dcfddc24173ab0
cyberbot777 <cyberbot7777777@gmail.com>
2024-05-27 13:22:28 -0500

    refactored python calc

---
+++ AddNumbers.py
@@ -0,0 +1,11 @@
+def add_numbers():
+    try:
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = num1 + num2
+        print(result)
+    except ValueError:
+        print("Invalid input! Please enter a number.")
+
+
+add_numbers()
